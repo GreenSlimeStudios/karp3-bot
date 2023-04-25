4e53352df120cb633321b2d8cb007bc11b99a308
Max Berliński <maxber.gdynia@gmail.com>
2023-04-25 18:13:55 +0200

    formatted the code

--- src/user.rs
+++ src/user.rs
@@ -1,57 +1,52 @@
-
 // use std::error::Error;
 // use sqlx::Connection;
 use sqlx::Row;
 
-
-pub struct DcUser{
-    pub id:String,
-    pub power:i64,
+pub struct DcUser {
+    pub id: String,
+    pub power: i64,
 }
 
-impl DcUser{
-    pub fn new(id:String)->Self{
-        Self{
-            id,
-            power:0,
-        }
+impl DcUser {
+    pub fn new(id: String) -> Self {
+        Self { id, power: 0 }
     }
 
-    pub fn moc(&self)->i64{
+    pub fn moc(&self) -> i64 {
         return self.power;
     }
-    
-    pub async fn get_user_data_or_create_user(&mut self,pool:&sqlx::PgPool){
 
-        let q = "SELECT * FROM db_users WHERE id = $1"; 
-        let query = sqlx::query(q)
-        .bind(&self.id);
-    
+    pub async fn get_user_data_or_create_user(&mut self, pool: &sqlx::PgPool) {
+        let q = "SELECT * FROM db_users WHERE id = $1";
+        let query = sqlx::query(q).bind(&self.id);
+
         let row = query.fetch_optional(pool).await.unwrap();
-        match row{
-            Some(v)=>{
+        match row {
+            Some(v) => {
                 self.power = v.get("power");
             }
-            None=>{
+            None => {
                 self.create_user(pool).await;
             }
         }
     }
-    pub async fn create_user(&self, pool: &sqlx::PgPool){
+    pub async fn create_user(&self, pool: &sqlx::PgPool) {
         let query = "INSERT INTO db_users (id, power) VALUES ($1, $2)";
         sqlx::query(query)
             .bind(&self.id)
             .bind(&self.power)
             .execute(pool)
-        .await.unwrap();
+            .await
+            .unwrap();
     }
-    pub async fn update_user(&self, pool: &sqlx::PgPool){
+    pub async fn update_user(&self, pool: &sqlx::PgPool) {
         // let query = "INSERT INTO dc_users (id, power) VALUES ($1, $2)";
         let query = "UPDATE db_users SET power = $2 WHERE id = $1";
         sqlx::query(query)
             .bind(&self.id)
             .bind(&self.power)
             .execute(pool)
-        .await.unwrap();
+            .await
+            .unwrap();
     }
 }
